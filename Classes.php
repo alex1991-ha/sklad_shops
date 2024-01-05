81f053ddfca4f332f079bc258fdc904acbbf0cc7
Alex Hembitski <alex1991_ha2@mail.ru>
2024-01-06 00:23:17 +0300

    07_01_24

--- Classes.php
+++ Classes.php
@@ -3,39 +3,37 @@
 
 class Table
 {
-	public function output_data($number, $product, $Quantity)
-	{
-		echo '
+    public function output_data($number, $product, $Quantity)
+    {
+        echo '
 			<tr>
         	<td>' . $number . '</td>
         	<td>' . $product . '</td>
         	<td id="centerUnits">шт</td>
             <td id="quantity" class="centerQuantity">' . $Quantity . '</td>
+            <td><input type="button" value="изменить" onclick="change()"></td>
       		</tr>';
-    
-	}
+    }
 
     public function output_data_in_popup()
-	{
+    {
         require('connect_db.php');
 
         $q = $db->query('SELECT * FROM products WHERE ID = 1');
         while ($row = $q->fetch()) {
-        $name = "$row[Product]";
-        $quantity = "$row[Quantity]";
-       
-     }
-		echo '
+            $name = "$row[Product]";
+            $quantity = "$row[Quantity]";
+        }
+        echo '
         <div id="popup">
             <div id="popup_body">
-                <form action="update.php" method="post">
-                    <p>Наименование: <input type="text" id="nameInPopup" value="'.$name.'" name="name"></p>
-                    <p>Количество: <input type="text" value='.$quantity.' name="quantity"></p>
+                <form action="update.php" method="POST">
+                    <p>Наименование: <input type="text" id="nameInPopup" value="' . $name . '" name="name"></p>
+                    <p>Количество: <input type="text" value=' . $quantity . ' name="quantity"></p>
                     <input type="button" value="Отмена" onclick="closeWindow()">
                     <input type="submit" value="ОК">
                 </form>
             </div>
         </div> ';
-    
-}
+    }
 }
--- script.js
+++ script.js
@@ -3,38 +3,10 @@ function change() {
   let window = document.getElementById("popup");
   window.style.visibility = "visible";
   window.style.opacity = 1;
-
-  //   let name = document.getElementById("nameInPopup");
-  //   let php = '<?php echo "hello"; ?>';
-  //   name.value = php;
 }
 
 function closeWindow() {
   let window1 = document.getElementById("popup");
   window1.style.visibility = "hidden";
   window1.style.opacity = 0;
-}
-
-function change_1() {
-  //кнопка минус
-  let minus = document.createElement("input");
-  minus.type = "button";
-  minus.id = "buttonMinus";
-  minus.value = "-";
-  let beforeMinus = document.getElementById("quantity");
-  // beforeMinus.appendChild(minus);
-  beforeMinus.before(minus);
-  // beforeMinus.insertBefore(minus, beforeMinus);
-  // document.getElementById("quantity").appendChild(minus);
-  //кнопка применить
-  let applay = document.createElement("input");
-  applay.type = "button";
-  applay.id = "buttonApply";
-  applay.value = "Применить";
-  applay.onclick = function appay() {
-    location.reload();
-  };
-  document.getElementById("buttons").appendChild(applay);
-  let del = document.getElementById("buttonCange");
-  del.remove();
 }
