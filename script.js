081798861faa326b1491dfc32a9d3ca46c29766e
Alex Hembitski <alex1991_ha2@mail.ru>
2024-01-09 01:13:26 +0300

    09_01_24

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-function change1() {
-  //вывод модального окна
-  let window = document.getElementById("popup");
-  window.style.visibility = "visible";
-  window.style.opacity = 1;
-  ("<?php $PC->output_data_in_popup(1); ?>");
-}
-function change2() {
-  //вывод модального окна
-  let window = document.getElementById("popup");
-  window.style.visibility = "visible";
-  window.style.opacity = 1;
-  ("<?php $PC->output_data_in_popup(2); ?>");
-}
+// function change1() {
+
+//   let window = document.getElementById("popup");
+//   window.style.visibility = "visible";
+//   window.style.opacity = 1;
+// }
+// function change2() {
+//   //вывод модального окна
+//   let window = document.getElementById("popup");
+//   window.style.visibility = "visible";
+//   window.style.opacity = 1;
+// }
 
 function closeWindow() {
   let window1 = document.getElementById("popup");
