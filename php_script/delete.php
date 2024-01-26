643afe13b4553670c7252f1a73841b47a3a8bd4c
alex1991-ha <alex1991_ha2@mail.ru>
2024-01-26 16:29:37 +0300

    26_01_2024

--- php_script/delete.php
+++ php_script/delete.php
@@ -5,9 +5,17 @@ require('../connect_db.php');
 $IDQuery = $_POST["ID"];
 
 $q = $db->query(
+    'SELECT ID
+     FROM products'
+);
+
+while ($row = $q->fetch()) {
+    $ID = "$row[ID]"; 
+  }
+
+$q = $db->query(
     'DELETE FROM products
      WHERE ID = ' . $IDQuery . ''
-
 );
 
 
